c94844d491b0a7ccfaa4f8fe34ae163345d6967d
haleyji <hyeyounghaleyji@gmail.com>
2022-12-19 17:44:29 +0900

    init commit

---
+++ src/main/java/com/younglog/YounglogApplication.java
@@ -0,0 +1,13 @@
+package com.younglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YounglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younglog/YounglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
