ae0b619d8fd564b01ac26a1ac5a7549d932b1294
haleyji <hyeyounghaleyji@gmail.com>
2022-12-29 11:13:52 +0900

    add asciidoc setting

--- src/test/java/com/younglog/controller/PostControllerDocTest.java
+++ src/test/java/com/younglog/controller/PostControllerDocTest.java
@@ -1,7 +1,9 @@
 package com.younglog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.younglog.domain.Post;
 import com.younglog.repository.PostRepository;
+import com.younglog.request.PostCreate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -25,6 +31,12 @@ import static java.nio.charset.StandardCharsets.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -37,6 +49,9 @@ public class PostControllerDocTest {
 
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -54,12 +69,40 @@ public class PostControllerDocTest {
                                         .build();
         postRepository.save(post);
 
-        mockMvc.perform(get("/posts/{postId}",1L)
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}",1L)
                         .contentType(APPLICATION_JSON)
                         .characterEncoding(UTF_8))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index"));
+                .andDo(document("post-inquiry",
+                        pathParameters(parameterWithName("postId").description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글 ID"),
+                                fieldWithPath("title").description("게시글 제목"),
+                                fieldWithPath("content").description("게시글 내용")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("글 등록")
+    void test2() throws Exception {
+        //given
+        PostCreate postCreate = PostCreate.builder().title("제목 하하하").content("내용 하하하").build();
+
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postCreate))
+                        .characterEncoding(UTF_8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").description("제목").attributes(key("constraint").value("좋은 제목 입력해주세요")),
+                                fieldWithPath("content").description("내용").optional()
+                           )
+                ));
     }
 }
 
